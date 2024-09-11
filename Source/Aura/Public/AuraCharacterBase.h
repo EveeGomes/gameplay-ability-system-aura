8ad93899aaf952655c8f062fe757eb2a3f83e45c
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-11 11:44:27 -0300

    Remove comments and undeclared function from cpp file

--- Source/Aura/Public/AuraCharacterBase.h
+++ Source/Aura/Public/AuraCharacterBase.h
@@ -6,20 +6,17 @@
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
-UCLASS(Abstract) // Adding Abstract specifier to UCLASS macro prevents this class from being dragged into the level.
+UCLASS(Abstract)
 class AURA_API AAuraCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this character's properties
-	AAuraCharacterBase();
+private:
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-
+	AAuraCharacterBase();
 
 };
