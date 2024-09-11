452017c7b4e284bc30b64f1a91500bbc5bad1ffa
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-11 12:34:50 -0300

    Attach the weapon to a socket on the mesh, and disable collision

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -16,7 +16,9 @@ AAuraCharacterBase::AAuraCharacterBase()
 
 	// Create the skeletal mesh component
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon"); // FName() doesn't need TEXT() macro, but FString does since it takes a string literal like that and makes a implicit conversion to a wide character string.
-
-
+	// Attach the weapon to a socket on the mesh
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	// Remove any collision from the weapon 
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
