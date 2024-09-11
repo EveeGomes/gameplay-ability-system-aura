52438f1edf4f4a744fe06b349c104ee3c47a7c43
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-11 12:33:22 -0300

    Construct the Weapon in the class constructor. Add comments regarding the usage of TEXT() macro

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -14,5 +14,9 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	// Create the skeletal mesh component
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon"); // FName() doesn't need TEXT() macro, but FString does since it takes a string literal like that and makes a implicit conversion to a wide character string.
+
+
 }
 
