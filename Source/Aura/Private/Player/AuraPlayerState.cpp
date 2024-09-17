2c4491f4c09815567534c8e4cd2b0009d44ddcab
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-17 15:09:20 -0300

    Declare and initialize GAS related pointers to PlayerState class.

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,8 +3,23 @@
 
 #include "Player/AuraPlayerState.h"
 
+/** GAS classes */
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
+   /** 
+   * Construct GAS related pointers:
+   */
+   // Construct AuraAbilitySystemComponent
+   AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+   // Make sure it is replicated
+   AbilitySystemComponent->SetIsReplicated(true);
+
+   // Construct AttributeSet
+   AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+
    /** 
    * Use NetUpdateFrequency to set how often the server tries to update clients. That's useful to sync up clients with the server version 
    *  as changes occur on ther server for the player state. So any variables that should be replicated, they'll update and the server will
--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -6,13 +6,26 @@
 #include "GameFramework/PlayerState.h"
 #include "AuraPlayerState.generated.h"
 
-/**
- * 
- */
+
+/** Forward Declaration */
+class UAbilitySystemComponent;
+class UAttributeSet;
+
 UCLASS()
 class AURA_API AAuraPlayerState : public APlayerState
 {
 	GENERATED_BODY()
+
+protected:
+	/** 
+	* Declare those pointers here since our player controlled character won't have them.
+	*/
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
+
 public:
 	AAuraPlayerState();
 };
