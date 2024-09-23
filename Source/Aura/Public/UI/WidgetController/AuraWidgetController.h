d91efd569d8782aa5bc144788638d2ed47a610cb
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-23 12:11:30 -0300

    Add member variables needed for retrieving data from

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -6,12 +6,37 @@
 #include "UObject/NoExportTypes.h"
 #include "AuraWidgetController.generated.h"
 
+class UAttributeSet;
+class UAbilitySystemComponent;
+
 /**
- * 
+ * This is our class responsible for getting data from the system, from the MODEL in our architecture, and then broadcast to our user widgets.
  */
 UCLASS()
 class AURA_API UAuraWidgetController : public UObject
 {
 	GENERATED_BODY()
 	
+	/** 
+	* Here we'll have a set of variables from which it's going to get any data.
+	* As for this game project, we're interested in 4 classes to retrieve data, so we'll have to have access to them:
+	*  - the ASC;
+	*  - the attribute set;
+	*  - the player state;
+	*  - the player controller
+	* We'll make these as protected variables, so children can have access to it.
+	*/
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<UAttributeSet> AttributeSet;
 };
