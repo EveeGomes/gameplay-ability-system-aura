845f4132b980922a8b45803b05f5f25ecbfff9cd
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-09-26 14:00:40 -0300

    Create delegates that will be assigned in BP.

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -3,3 +3,8 @@
 
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+   // No need to call super since it's empty
+
+}
--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -6,6 +6,17 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
+/** 
+* Add/Make dynamic multicast delegates to
+* Dynamic so that we can assign events in in BP (in our widget BP specifically);
+* Multicast because multiple BPs, widget BPs, may want to bind to these delegates so that they can update.
+*/
+// These can send a single parameter. It can broadcast a float.
+// Below we're just declaring the type, we'll need some member variables of these types so then we can assign in BP.
+/** Delegates types */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
+
 /**
  * 
  */
@@ -14,4 +25,13 @@ class AURA_API UOverlayWidgetController : public UAuraWidgetController
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void BroadcastInitialValues() override;
+
+	/** Delegates themselves */
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FOnHealthChangedSignature OnHealthChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnMaxHealthChangedSignature OnMaxHealthChanged;
 };
