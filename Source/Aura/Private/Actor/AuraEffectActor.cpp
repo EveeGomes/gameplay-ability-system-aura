320e80b113f5bdf13a2f88c4943247c9c3cd455c
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-10-07 11:24:03 -0300

    L41. correct some comments

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -12,14 +12,14 @@ AAuraEffectActor::AAuraEffectActor()
 	PrimaryActorTick.bCanEverTick = false;
 
 	/** 
-	* Both Static mesh and the Sphere we'll be adding in the designer domain, i.e. on Blueprint side, instead of here in C++. That's because the
+	* Both Static mesh and the Sphere we'll be added in the designer domain, i.e. on Blueprint side, instead of here in C++. That's because the
 	*  visual representation of the actor (static mesh) and the shape of overlap sphere could be determined by designers (works more on the BP side).
 	*  More technical details can be determined here in C++.
 	* 
 	* As we remove the static mesh and sphere from C++, we'll also remove the overlap callback functions and the binding lines. We'll do that in order
 	*  to give the designers side some control on how this actor should work. We should make this actor flexible and easily configurable on the BP side.
 	*  So, if we want to add a mesh, or box, or sphere component, we can do that and add it to the root component. For that we'll set the root component
-	*  to a USceneComponent we created:
+	*  to a USceneComponent created here:
 	*/
 
 	// Create a USceneComponent and assign it to the root component.
