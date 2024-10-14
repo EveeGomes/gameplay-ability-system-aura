3a83c384fa5a1385c61f4a98e4abd03009b5d775
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-10-14 11:11:36 -0300

    Add some comments on the use of PlayerState

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -14,6 +14,13 @@
 class UAbilitySystemComponent;
 class UAttributeSet;
 
+/** 
+* This is the most important class for shared information about a specific player. It's meant to hold current info about the player, and each player
+*  has their own PS. https://cedric-neukirchen.net/docs/multiplayer-compendium/common-classes/playerstate/
+* https://gascompanion.github.io/asc-on-player-state/
+* This is where the ASC and AttributeSet(AS) are going to live for the player controlled character (Aura). For the enemies, both ASC and AS are going
+*  to live on the enemy class itself! (Check class 17 for the reasons to put ASC and AS in this class for Aura).
+*/
 UCLASS()
 class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
 {
