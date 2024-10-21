00266b2fa09375b63dea8068cfc3b86fe6cb0c08
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-10-21 11:33:36 -0300

    L55. Get tags from GEs which were added to them in their BP

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -12,6 +12,26 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-   // For testing:
-   GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("EffectApplied!"));
+   /** 
+   * To show things in the HUD we need to know about our dependencies.
+   * AuraASC know nothing about our WidgetController, but the WidgetController is what broadcasts data to the widgets! Now, if we want to show something
+   *  to the screen, that's in the VIEW domain, which receives data from the Controller domain, which consequently receives info from the ASC, so our ASC
+   *  should have a delegate. The question is: what our delegate is going to broadcast? What information do we want? A good way to identify things are with
+   *  Gameplay Tags! So, if an effect carries a GT, we can broadcast that to the WidgetController, which can then broadcast it to the HUD!
+   * So, here we'll get some gameplay tags from the GEs, and broadcast them in a delegate to use them to display things to the screen.
+   */
+
+   // Get a GT: create a container that will carry all the tags
+   FGameplayTagContainer TagContainer;
+   EffectSpec.GetAllAssetTags(TagContainer);
+
+   // Broadcast the GTs associated with the GE (we added those tags in the GE BP)
+   for (const FGameplayTag& Tag : TagContainer)
+   {
+      // TODO: Broadcast the tag to the WidgetController
+      // format a string before passing as a debug message. By using ToString method in Tag, we need to use * so the string is converted to
+      //  a wide character array (array of TChars to be exact)
+      const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+      GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+   }
 }
