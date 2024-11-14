d6bdd69030b5f07546b1c58d7952a42974e31c2d
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-11-14 10:27:06 -0300

    L71 Remove initialization of MaxHealth and MaxMana as those are now driven by an infinite GE. Also uncomment clamping in pre and post GE change

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -15,9 +15,7 @@ UAuraAttributeSet::UAuraAttributeSet()
 {
    // Initialize Attributes
    InitHealth(10.f);
-   InitMaxHealth(100.f);
    InitMana(10.f);
-   InitMaxMana(50.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
@@ -74,12 +72,12 @@ void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, 
    if (Attribute == GetHealthAttribute())
    {
       // Avoid Health to ever go above MaxHealth value or below 0
-      //NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+      NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
    }
    if (Attribute == GetManaAttribute())
    {
       // Avoid Mana to ever go above MaxMana or below 0
-      //NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+      NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
    }
 }
 
@@ -154,12 +152,12 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
    // Clamp Health
    if (Data.EvaluatedData.Attribute == GetHealthAttribute())
    {
-      //SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+      SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
    }
    // Clamp Mana
    if (Data.EvaluatedData.Attribute == GetManaAttribute())
    {
-      //SetMana(FMath::Clamp(GetMana(), 0.f, GetMaxMana()));
+      SetMana(FMath::Clamp(GetMana(), 0.f, GetMaxMana()));
    }
 }
 
