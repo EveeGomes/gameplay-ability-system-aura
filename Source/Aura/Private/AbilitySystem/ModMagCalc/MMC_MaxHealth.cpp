856ade88b42e86b5e7ab8d8e1419f26bc141def0
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-11-19 13:44:28 -0300

    L74 Add override declaration of CalculateBaseMagnitude_Implementation, and declare a variable used for the attribute we want to capture in the MMC class.

--- Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
@@ -3,3 +3,11 @@
 
 #include "AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
 
+UMMC_MaxHealth::UMMC_MaxHealth()
+{
+}
+
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+   return 0.0f;
+}
--- Source/Aura/Public/AbilitySystem/ModMagCalc/MMC_MaxHealth.h
+++ Source/Aura/Public/AbilitySystem/ModMagCalc/MMC_MaxHealth.h
@@ -7,11 +7,30 @@
 #include "MMC_MaxHealth.generated.h"
 
 /**
- * 
+ * MaxHealth will be determined by the calculations created in this class!
  */
 UCLASS()
 class AURA_API UMMC_MaxHealth : public UGameplayModMagnitudeCalculation
 {
 	GENERATED_BODY()
-	
+
+public:
+	UMMC_MaxHealth();
+
+	/** Begin UGameplayModMagnitudeCalculation */
+	/**
+	* This function returns a float which is the result that our modifier should produce. So all the calculations that result in that value, we'll
+	*  determine in this function!
+	* The param @Spec is the Effect Spec of whatever GE that has a modifier using this custom calculation. I.E. as we have access to that GE spec,
+	*  we have access to all information inside of that.
+	*/
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+	/** End UGameplayModMagnitudeCalculation */
+
+private:
+	/** 
+	* ModMagCalc classes are also able to capture attributes. To do that we have to create a variable for the attribute we want to capture, and the
+	*  type is FGameplayEffectAttributeCaptureDefinition.
+	*/
+	FGameplayEffectAttributeCaptureDefinition VigorDef;
 };
