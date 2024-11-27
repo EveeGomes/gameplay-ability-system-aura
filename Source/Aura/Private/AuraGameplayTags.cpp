914a1f584eaaf262be245acf52950047cfd131b1
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2024-11-27 17:35:19 -0300

    L86 Create a class to behave as a singleton for GTs. GTs will be stored there.

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Copyright Eveline Gomes.
+
+
+#include "AuraGameplayTags.h"
+
+AuraGameplayTags::AuraGameplayTags()
+{
+}
+
+AuraGameplayTags::~AuraGameplayTags()
+{
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,15 @@
+// Copyright Eveline Gomes.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * This class is used to store the GTs. This results in storing the GTs in a centralized location. So it'll be our singleton for GTs.
+ */
+class AURA_API AuraGameplayTags
+{
+public:
+	AuraGameplayTags();
+	~AuraGameplayTags();
+};
