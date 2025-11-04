440946d006aa8354b17cdbdb828b0b9298f9542a
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2025-11-04 07:56:44 -0300

    L86 Define and declare another static method to initialize the gameplay tags.

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -3,5 +3,30 @@
 
 #include "AuraGameplayTags.h"
 
+/** Use the GT manager */
+#include "GameplayTagsManager.h"
+
 /** For a static variable, we need to explicitly declare the type. We have to declare the variable fully qualified */
 FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	/**
+	 * When a GT is created, it's registered with the gameplay tag manager (only one for the whole project).
+	 * Get the GT manager by calling a getter function, that returns the one and only instance of that manager, since it's a singlenton!
+	 * Then, we can have access to many function such as AddNativeGameplayTag that allows us to declare GTs.
+	 *
+	 * This method we're defining here, InitializeNativeGameplayTags(), should be called early on in the project.
+	 *	It has to be as soon as we can before any of those tags are attempted to be used!
+	 *  To achieve that, we'll create yet another singleton, our version of an asset manager that'll have to do with loading
+	 *	primary assets, and it's where we can initialize this GTs.
+	 *	
+	 */
+
+	UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.Armor"),
+		FString("Reduces damage taken, improves Block Chance.")
+		);
+
+	
+}
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -9,8 +9,8 @@
 
 /**
  * This class is used to store the GTs. This results in storing the GTs in a centralized location. So it'll be our singleton for GTs.
- * It'll actually be changed to a struct, so the class declaration has been deleted and we declare now a struct instead.
- * So in summary this is a Singleton containing native Gameplay Tags (native in a sense that they're created from within C++ and available in C++. 
+ * It'll actually be changed to a struct, so the class declaration has been deleted, and we declare now a struct instead.
+ * In summary, this is a Singleton containing native Gameplay Tags (native in a sense that they're created from within C++ and available in C++. 
  *  They'll be available in the editor/BP as well!).
  * 
  * Since it's a singleton, we have to create a static getter function, so we don't need an instance of this class to exist to be able to call
@@ -24,8 +24,14 @@ struct FAuraGameplayTags
 public:
    /** Returns a const reference to the one and only instance of AuraGameplayTags that'll exist in the game. */
    static const FAuraGameplayTags& Get() { return GameplayTags; }
+
+   /** Initialize (create) the GTs. */
+   static void InitializeNativeGameplayTags();
+   
 protected:
+   
 private:
+   /** One and only instance of this struct. */
    static FAuraGameplayTags GameplayTags;
 };
 
