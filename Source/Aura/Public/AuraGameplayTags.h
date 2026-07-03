69b6fe4b8df04731361e927993c2ae9085a9da47
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2026-07-03 17:02:58 -0300

    L88 add primary attributes as as well

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -29,17 +29,25 @@ public:
    static void InitializeNativeGameplayTags();
    
    /** Gameplay Tags Variables */
+
+   ///////////////////////// Primary Attributes /////////////////////////
+   FGameplayTag Attributes_Primary_Strength;
+   FGameplayTag Attributes_Primary_Intelligence;
+   FGameplayTag Attributes_Primary_Resilience;
+   FGameplayTag Attributes_Primary_Vigor;
+   ///////////////////////// Primary Attributes /////////////////////////
+   
    ///////////////////////// Secondary Attributes /////////////////////////
    FGameplayTag Attributes_Secondary_Armor;
-   FGameplayTag Attributes_Secondary_Armor_Penetration;
-   FGameplayTag Attributes_Secondary_Block_Chance;
-   FGameplayTag Attributes_Secondary_Critical_Hit_Chance;
-   FGameplayTag Attributes_Secondary_Critical_Hit_Damage;
-   FGameplayTag Attributes_Secondary_Critical_Hit_Resistance;
-   FGameplayTag Attributes_Secondary_Health_Regeneration;
-   FGameplayTag Attributes_Secondary_Mana_Regeneration;
-   FGameplayTag Attributes_Secondary_Max_Health;
-   FGameplayTag Attributes_Secondary_Max_Mana;
+   FGameplayTag Attributes_Secondary_ArmorPenetration;
+   FGameplayTag Attributes_Secondary_BlockChance;
+   FGameplayTag Attributes_Secondary_CriticalHitChance;
+   FGameplayTag Attributes_Secondary_CriticalHitDamage;
+   FGameplayTag Attributes_Secondary_CriticalHitResistance;
+   FGameplayTag Attributes_Secondary_HealthRegeneration;
+   FGameplayTag Attributes_Secondary_ManaRegeneration;
+   FGameplayTag Attributes_Secondary_MaxHealth;
+   FGameplayTag Attributes_Secondary_MaxMana;
    ///////////////////////// Secondary Attributes /////////////////////////
    
 protected:
